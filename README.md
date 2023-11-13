9a534f2250813afabfe7319b0830029e37df9c0b
jamisousa <jamiledsousa@gmail.com>
2023-11-13 15:33:44 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Projeto final: Desenvolvimento de Chatbot com Análise de Sentimento
+Objetivo:
+Desenvolver um chatbot capaz de interagir com usuários, compreendendo e respondendo a mensagens de forma contextualmente relevante, utilizando técnicas de processamento de linguagem natural, com ênfase na análise de sentimentos.
+Etapas do Projeto:
+Definição do Escopo
+Os alunos identificam o propósito do chatbot (por exemplo, suporte ao cliente, assistente pessoal, repositório de informação, entretenimento, etc.).
+Coleta de Dados
+Os alunos coletam dados de treinamento (base de conhecimento), incluindo diálogos e mensagens de usuários relevantes ao escopo do chatbot.É possível utilizar conjuntos de dados públicos (kaggle) ou criar cenários próprios, com informações recorrentes (FAQ).
+Pré-processamento e Análise de Sentimento
+Implementação de técnicas de pré-processamento de texto.Integração de um módulo de análise de sentimento (NLTK ou TF-IDF) para entender a polaridade emocional das mensagens recebidas.
+Treinamento do Modelo de Linguagem
+Treinamento do modelo utilizando os dados coletados.
+Integração da Análise de Sentimento no Chatbot
+Os alunos integram o módulo de análise de sentimento no fluxo de processamento do chatbot.Desenvolvimento de lógica para ajustar respostas com base na análise de sentimento.
+Desenvolvimento da Interface de Usuário
+Implementação de uma interface de usuário amigável para interação com o chatbot.Considerar a possibilidade do usuário ajustar o humor das respostas do Chatbot
+Testes e Avaliação
+Testes do chatbot em diferentes cenários.Avaliação da eficácia da análise de sentimento em relação às respostas geradas.
+Otimização e Ajustes
+Ajustes no modelo e na lógica do chatbot com base nos resultados dos testes.Otimização da performance e eficiência.
+Apresentação Final
+Apresentação do chatbot em funcionamento, destacando os recursos de análise de sentimento e interatividade.Discussão sobre desafios enfrentados, lições aprendidas e eficiência do modelo.
---
+++ chatbot/chatbot.py
@@ -0,0 +1,105 @@
+import spacy
+import pandas as pd
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Carregar o modelo do spaCy
+nlp = spacy.load('en_core_web_sm')
+
+# Função de pré-processamento
+def preprocessing(sentence):
+    if isinstance(sentence, str):  # Verificar se é uma string não nula
+        # Remover nomes de usuários
+        sentence = re.sub(r'@[A-Za-z0-9]+', ' ', sentence)
+        # Remover sites
+        sentence = re.sub(r'https?://[A-Za-z0-9./]+', ' ', sentence)
+
+        sentence = sentence.replace('.', '')
+        sentence = sentence.lower()
+        
+        # Remover stop words antes da lematização
+        tokens = [token.lemma_ for token in nlp(sentence) if not token.is_stop]
+        
+        tokens = ' '.join([element for element in tokens])
+        return tokens
+    else:
+        return ''  # Retorna uma string vazia se não for uma string não nula
+
+# Carregar os dados
+train_data = pd.read_csv(r'archive\Reddit_Data.csv', header=None, names=['clean_comment', 'category'], encoding='latin1')
+
+# Remover linhas com valores nulos
+train_data = train_data.dropna(subset=['clean_comment'])
+
+# Separar a base em x e y
+x = train_data['clean_comment'].values
+y = train_data['category'].values
+
+# Reduzir a quantidade de linhas na base (para processar mais rápido)
+x, _, y, _ = train_test_split(x, y, test_size=0.80)
+
+# Subdividir a base em treinamento e teste
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.2)
+
+print("Preprocessing...")
+# Pode demorar cerca de 5 min
+x_train_cleaned = [preprocessing(comment) for comment in x_treino]
+x_test_cleaned = [preprocessing(comment) for comment in x_teste]
+
+print("Número de documentos vazios após pré-processamento:", sum(len(doc.split()) == 0 for doc in x_train_cleaned))
+
+# Passo 1 - Transformar x em tf-idf
+vectorizer = TfidfVectorizer()
+x_train_tfidf = vectorizer.fit_transform(x_train_cleaned)
+
+# Passo 2 - Otimizar os vetores
+def preprocessing_lemma(sentence):
+    tokens = [token.lemma_ for token in nlp(sentence)]
+    tokens = ' '.join(element for element in tokens)
+    return tokens
+
+x_train_cleaned_lemma = [preprocessing_lemma(comment) for comment in x_train_cleaned]
+x_train_tfidf = vectorizer.fit_transform(x_train_cleaned_lemma)
+
+x_test_cleaned_lemma = [preprocessing_lemma(comment) for comment in x_test_cleaned]
+x_test_tfidf = vectorizer.transform(x_test_cleaned_lemma)
+
+# Passo 3 - Montar a árvore
+model = DecisionTreeClassifier(criterion='entropy')
+model.fit(x_train_tfidf, y_treino)
+
+# Passo 4 - Avaliar a qualidade da árvore
+predictions = model.predict(x_test_tfidf)
+accuracy = accuracy_score(y_teste, predictions)
+conf_matrix = confusion_matrix(y_teste, predictions)
+
+print("Accuracy:", accuracy)
+print("Confusion Matrix:\n", conf_matrix)
+
+#Função para avaliar uma nova sentença
+def avalie(sentence):
+    sentence_cleaned = preprocessing_lemma(preprocessing(sentence))
+    sentence_tfidf = vectorizer.transform([sentence_cleaned])
+    prediction = model.predict(sentence_tfidf)
+
+    #Conversão de categoria para str
+    prediction_as_str = str(prediction[0])
+
+    if prediction_as_str == '0':
+        print('frase neutra')
+    elif prediction_as_str == '1':
+        print('frase positiva')
+    elif prediction_as_str == '-1':
+        print('frase negativa')
+
+    #Depuração
+    print("Categoria Real:", y_teste[0]) 
+    print("Categoria Prevista:", prediction_as_str)
+    print("Categorias Reais:", y_teste)
+    print("Categorias Previstas:", predictions)
+
+        
+avalie('I LOVE IT!')
